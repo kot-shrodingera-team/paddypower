328474d097c2abdf06866bdeae29b35b98d17136
kot-shrodingera <kot.shrodingera@gmail.com>
2021-08-26 14:08:39 +0300

    feat: безцикленный переход на новое событие

--- src/show_stake/openEvent.ts
+++ src/show_stake/openEvent.ts
@@ -1,16 +1,42 @@
 import { log } from '@kot-shrodingera-team/germes-utils';
-import { NewUrlError } from '@kot-shrodingera-team/germes-utils/errors';
+import { JsFailError, NewUrlError } from '@kot-shrodingera-team/germes-utils/errors';
 
 const openEvent = async (): Promise<void> => {
   // soccer => football ?
   const eventUrl = worker.EventUrl.replace('/soccer/', '/football/');
+
+  /* ======================================================================== */
+  /*             Если не было попытки перехода на страницу события            */
+  /* ======================================================================== */
+
+  if (
+    worker.GetSessionData(
+      `${window.germesData.bookmakerName}.TransitionToEventPage`
+    ) === '0'
+  ) {
+    if (window.location.href.startsWith(eventUrl)) {
+      log('Уже открыто нужное событие', 'steelblue');
+      return;
+    }
+    log(`${window.location.href} !== ${eventUrl}`, 'white', true);
+    worker.SetSessionData(
+      `${window.germesData.bookmakerName}.TransitionToEventPage`,
+      '1'
+    );
+    window.location.href = eventUrl;
+    throw new NewUrlError('Переходим на событие');
+  }
+
+  /* ======================================================================== */
+  /*              Если была попытка перехода на страницу события              */
+  /* ======================================================================== */
+
   if (window.location.href.startsWith(eventUrl)) {
-    log('Уже открыто нужное событие', 'steelblue');
+    log('Открыли нужное событие', 'steelblue');
     return;
   }
-  log(`${window.location.href} !== ${eventUrl}`, 'white', true);
-  window.location.href = eventUrl;
-  throw new NewUrlError('Переходим на событие');
+  log(`${window.location.href} !== ${eventUrl}`, 'white');
+  throw new JsFailError('Не удалось перейти на нужное событие');
 };
 
 export default openEvent;
